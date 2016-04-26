dd480e90ef2e644bcbf491f7e295c3bdd9333449
MacKentoch <erwan.datin@gmail.com>
2016-04-26 17:26:33 +0200

    chore : SearchForm

--- src/app/components/topNavBar/searchForm/SearchForm.jsx
+++ src/app/components/topNavBar/searchForm/SearchForm.jsx
@@ -6,9 +6,15 @@ class SearchForm extends Component {
 
   handleKeyPress(target) {
     if (target.charCode === 13) {
-      const searchInput = this.refs.searchInput;
+      const searchInput = this.refs.searchInput.findDOMNode();
       this.props.submitSearch(searchInput.value);
     }
+  }
+
+  handleOnClick(event) {
+    event.preventDefault();
+    console.log('this.refs.searchInput.style', this.myTextInput);
+     this.myTextInput.style.width = '300px';
   }
 
   render() {
@@ -18,12 +24,14 @@ class SearchForm extends Component {
     return (
       <form className={formClass}>
         <input
-          refs="searchInput"
+          ref={(ref) => this.myTextInput = ref}
+
           type="text"
           className="form-control searchInputLarge"
           autoFocus={props.autofocus}
           placeholder={props.placeHolder}
           onKeyPress={(target) => this.handleKeyPress(target)}
+          onClick={(ev) => this.handleOnClick(ev)}
         />
       </form>
     );
