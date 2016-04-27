a30ca95c5a73e8f4ae5e1f6f36466ebaae7d45b4
MacKentoch <erwan.datin@gmail.com>
2016-04-27 07:53:41 +0200

    chore : clean

--- src/app/containers/index.js
+++ src/app/containers/index.js
@@ -1,9 +1,7 @@
 import App            from './app/App.jsx';
-import ViewsContainer from './viewsContainer/ViewsContainer.jsx';
 import PageContainer  from './pageContainer/PageContainer.jsx'
 
 export {
   App,
-  ViewsContainer,
   PageContainer
 };
